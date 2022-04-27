06efa46d320ca47f7402e12557018c37e2469b13
Nickolay Leonov <nickolayshmakolay@gmail.com>
2022-04-27 20:17:33 +0300

    Bot-App: All available menu items are storing in BotCommandsDAO class.

--- BotApp/src/main/java/ru/bandit/cryptobot/bot/BotRequestHandler.java
+++ BotApp/src/main/java/ru/bandit/cryptobot/bot/BotRequestHandler.java
@@ -3,19 +3,16 @@ package ru.bandit.cryptobot.bot;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 import ru.bandit.cryptobot.bot.menu.AbstractMenuItem;
 import ru.bandit.cryptobot.bot.menu.MenuError;
+import ru.bandit.cryptobot.dao.BotCommandsDAO;
 import ru.bandit.cryptobot.dto.QueryDTO;
 import ru.bandit.cryptobot.dto.UserDTO;
 import ru.bandit.cryptobot.exceptions.CommonBotAppException;
-import ru.bandit.cryptobot.exceptions.QueryException;
 import ru.bandit.cryptobot.services.QueryService;
 
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * This class handle users request and making response to it.
@@ -25,20 +22,11 @@ public class BotRequestHandler {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    private final Map<String, AbstractMenuItem> availableCommands;
-
     @Autowired
     private QueryService queryService;
 
     @Autowired
-    public BotRequestHandler(ApplicationContext context) {
-        //Getting all available menu items from application context.
-        //Assuming that all menu items must be child to AbstractMenuItem abstract class
-        this.availableCommands = context.getBeansOfType(AbstractMenuItem.class)
-                .entrySet()
-                .stream()
-                .collect(Collectors.toMap(menuEntry -> menuEntry.getValue().getCommandName(), Map.Entry::getValue));
-    }
+    private BotCommandsDAO botCommandsDAO;
 
     /**
      * Method makes {@link AbstractMenuItem} object as response to users request.
@@ -57,7 +45,7 @@ public class BotRequestHandler {
             String commandName = separatedQuery.remove(0);
 
             //trying to find command in command map
-            AbstractMenuItem foundMenuItem = this.findMenuItem(commandName);
+            AbstractMenuItem foundMenuItem = botCommandsDAO.findMenuItem(commandName);
 
             //creating queryDTO object
             //todo decide if queryDTO is not needed
@@ -75,23 +63,6 @@ public class BotRequestHandler {
         } catch (CommonBotAppException e) {
             logger.debug(e.getMessage());
             return new MenuError(e);
-        }
-
-    }
-
-    /**
-     * Get correct menu item by its name.
-     *
-     * @param command name of requested item
-     * @return found menu item.
-     * @throws CommonBotAppException if requested menu item is not found.
-     */
-    private AbstractMenuItem findMenuItem(String command) throws CommonBotAppException {
-        AbstractMenuItem foundItem = this.availableCommands.get(command);
-        if (foundItem == null) {
-            throw new QueryException("Requested command not found.", QueryException.ExceptionCause.COMMAND_NOT_FOUND);
-        } else {
-            return foundItem;
         }
     }
 }
